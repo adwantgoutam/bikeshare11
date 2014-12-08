c1078b365f1049d031df7b6a2978e675b16bb292
nikhilpanicker <nikhil.panicker@sjsu.edu>
2014-12-08 02:15:14 -0800

    Changes done for UI Registration

--- client/BikeShare/src/com/team11/bikeshare/Registration.java
+++ client/BikeShare/src/com/team11/bikeshare/Registration.java
@@ -3,25 +3,24 @@ package com.team11.bikeshare;
 
 
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.AsyncHttpResponseHandler;
-import com.loopj.android.http.RequestParams;
-import com.team11.beans.User;
-import com.team11.beans.UserContext;
-
-
-import android.os.AsyncTask;
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.team11.beans.Bikes;
+import com.team11.beans.User;
+import com.team11.beans.UserContext;
 
 public class Registration extends Activity{
 	private EditText user_name;
@@ -31,6 +30,8 @@ public class Registration extends Activity{
 	private EditText bikeModel;
 	private EditText email;
 	private CheckBox checkbox;
+	private EditText SSN;
+	
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -45,6 +46,7 @@ public class Registration extends Activity{
 		bikeName=(EditText)findViewById(R.id.bikeName);
 		bikeModel=(EditText)findViewById(R.id.bikeModel);
 		checkbox = (CheckBox) findViewById(R.id.isBike);
+		SSN = (EditText)findViewById(R.id.SSN);
 		checkbox.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -73,35 +75,56 @@ public class Registration extends Activity{
     		user.setUser_name(user_name.getText().toString());
     		user.setMobile_number(mobile.getText().toString());
     		user.setPassword(paswd.getText().toString());
+    		user.setSsn(SSN.getText().toString());
+    		//user.setSsn(ssn.)
        		uc.setUser(user);
+       		Bikes bike = new Bikes();
+       		bike.setIsBikeAvailable("Yes");
+       		bike.setPincode("95112");
+       		bike.setBike_id(bikeName.getText().toString());
+       		bike.setBikeModel(bikeModel.getText().toString());
+       		bike.setUser_id(user_name.getText().toString());
+ 
+       		uc.setBikes(bike);
+       		//bike.se
     		GsonBuilder builder = new GsonBuilder();
             Gson gson = builder.create();
             String str=gson.toJson(uc);
             params.put("userContext", str);
     		//DataObject obj = gson.fromJson(br, DataObject.class);
 
-    		client.post("http://10.0.0.9:8080/register_user",params, new AsyncHttpResponseHandler(){
+    		//client.post("http://10.0.0.23:8080/register_user",params, new AsyncHttpResponseHandler(){
+            client.post("http://192.168.2.9:8080/register_user",params, new AsyncHttpResponseHandler(){
     			
     			public void onSuccess(int statuscode,String response)
     			{
-    				Toast.makeText(getApplicationContext(), statuscode+"Success registration", Toast.LENGTH_LONG).show();
-    					if(response.contains("User Registered Successfully"))
+    				System.out.println("Response: "+response);
+    				//Toast.makeText(getApplicationContext(), statuscode+"Success registration", Toast.LENGTH_LONG).show();
+    					if(response.contains("Success"))
     					{
+    						//Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_LONG).show();
+    						Intent in = new Intent().setClass(getApplicationContext(), Home.class);
+    						String userid=user_name.getText().toString();
+    						in.putExtra("userid", userid);
+    						startActivity(in);
+    						
         					    					}
     					else
     					{
-    						Toast.makeText(getApplicationContext(), "Please Try Again", Toast.LENGTH_LONG).show();
+    						Toast.makeText(getApplicationContext(), "Username Exist! Please choose anothe UserName. Please Use another UserName", Toast.LENGTH_LONG).show();
 
     					}
     				
     			}
     		}
     		);
+    		/*
     		Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_LONG).show();
-			Intent in = new Intent().setClass(getApplicationContext(), ShowBikeLocations.class);
+			Intent in = new Intent().setClass(getApplicationContext(), Home.class);
 			String userid=user_name.getText().toString();
 			in.putExtra("userid", userid);
 			startActivity(in);
+			*/
 
 
 
