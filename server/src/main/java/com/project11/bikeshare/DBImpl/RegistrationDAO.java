90709343b3257a993e998ab91d79ee4196aa5284
nikhilpanicker <nikhil.panicker@sjsu.edu>
2014-12-08 23:43:17 -0800

    Changed from basicDBO to Jongo

--- server/src/main/java/com/project11/bikeshare/DBImpl/RegistrationDAO.java
+++ server/src/main/java/com/project11/bikeshare/DBImpl/RegistrationDAO.java
@@ -12,6 +12,7 @@ import org.jongo.MongoCollection;
 
 
 
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.mongodb.BasicDBObject;
@@ -21,12 +22,14 @@ import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
+import com.project11.bikeshare.Beans.Bikes;
 import com.project11.bikeshare.Beans.User;
 import com.project11.bikeshare.Beans.UserContext;
 import com.project11.bikeshare.Beans.UserFeedback;
 
 public class RegistrationDAO extends BikeShareDB{
 
+	/*
 	public void registerUser(UserContext userContext){
 		//save this user in mongo
 		System.out.println("Inserting user");
@@ -35,19 +38,61 @@ public class RegistrationDAO extends BikeShareDB{
 		
 		
 	}
+	*/
+	
+	public String registerUser(UserContext userContext){
+		//save this user in mongo
+		System.out.println("Inserting user");
+		
+		User userDetails = userCollectionJongo.findOne("{username:'"+ userContext.getUser().getUser_name()+"'}").as(User.class);
+		
+		if (userDetails==null)
+		{
+			System.out.println("Inside null");
+			userCollectionJongo.save(userContext.getUser());
+			/*
+			BasicDBObject doc=new BasicDBObject();
+			
+			doc.put("username",userContext.getUser().getUser_name());
+			doc.put("password",userContext.getUser().getPassword());
+			doc.put("email",userContext.getUser().getEmail_id());
+			doc.put("mobilenumber",userContext.getUser().getMobile_number());
+			doc.put("ssn",userContext.getUser().getSsn());
+
+			userCollectionJongo.save(doc);
+			*/
+			bikesCollectionJongo.save(userContext.getBike());
+			/*
+			BasicDBObject bikeDoc=new BasicDBObject();
+			bikeDoc.put("user_id",userContext.getBike().getUser_id());
+			bikeDoc.put("isbikeavailable",userContext.getBike().getIsBikeAvailable());
+			bikeDoc.put("bikeid",userContext.getBike().getBike_id());
+			bikeDoc.put("pincode",userContext.getBike().getPincode());
+			bikeDoc.put("bike_model",userContext.getBike().getBikeModel());
+			bikesCollectionJongo.save(bikeDoc);
+			*/
+			return "Success";
+		}
+		else
+		{
+			System.out.println("Inside nt null");			
+			return "Username Exist! Please choose anothe UserName";
+		}
+		
+	}
 
 	public User login(String user) throws UnknownHostException{
 		MongoClient client = new MongoClient(new ServerAddress("ds051160.mongolab.com",51160));
 		DB database = client.getDB("bikeshare");
 	    database.authenticate("bikeshare","bikeshare".toCharArray());
 	    DBCollection collection = database.getCollection("user");
-	    BasicDBObject query = new BasicDBObject("user_name",user);
+	    BasicDBObject query = new BasicDBObject("username",user);
 	    DBCursor cursor = collection.find(query);
 	    User u=new User();
 	    while(cursor.hasNext())
 	    {
 	    	DBObject obj=cursor.next();
-	    	u.setUsername(String.valueOf(obj.get("user_name")));
+	    	u.setUser_name(String.valueOf(obj.get("username")));
 	    	u.setPassword(String.valueOf(obj.get("password")));
 	    }
 	     return u;		
@@ -59,10 +104,5 @@ public class RegistrationDAO extends BikeShareDB{
 		System.out.println("Inserting feedback");
 		 feedbackCollectionJongo.save(uf);
 	}
-	
-	public UserFeedback getUserFeedback(String user_id_renter) {
-		
-		 feedbackCollectionJongo.save(user_id_renter);
-		 return null;
-	}
+
 }
